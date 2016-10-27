cb2b2a6ffa934abf559f8504c50d5611b202b1e4
zhenfei.yu <zhenfei.yu@utbm.fr>
2016-10-27 06:45:46 -0400

    forecast

--- test/controllers/cities_controller_test.rb
+++ test/controllers/cities_controller_test.rb
@@ -4,33 +4,35 @@ class CitiesControllerTest < ActionController::TestCase
   setup do
     @city = cities(:one)
   end
-
+  
   test "should get index" do
     VCR.use_cassette("forecast") do
       get :index
       assert_response :success
-     assert_not_nil assigns(:cities)
+      assert_not_nil assigns(:cities)
     end
   end
+
 
   test "should get new" do
     get :new
     assert_response :success
   end
-  
+
   test "should create city" do
-    VCR.use_cassette("nominatim") do
-      assert_difference('City.count') do
-        post :create, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
-      end
-      assert_redirected_to city_path(assigns(:city))
+   VCR.use_cassette("nominatim") do
+    assert_difference('City.count') do
+      post :create, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
     end
+    end
+    assert_redirected_to city_path(assigns(:city))
   end
+
 
   test "should show city" do
     VCR.use_cassette("forecast") do
-      get :show, id: @city
-     assert_response :success
+       get :show, id: @city
+       assert_response :success
     end
   end
 
@@ -45,7 +47,6 @@ class CitiesControllerTest < ActionController::TestCase
       assert_redirected_to city_path(assigns(:city))
     end
   end
-
   test "should destroy city" do
     assert_difference('City.count', -1) do
       delete :destroy, id: @city
