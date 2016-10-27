a7d38b4c8dd92bfb62a31d83d67c6d17d6f1048a
zhenfei.yu <zhenfei.yu@utbm.fr>
2016-10-27 06:26:52 -0400

    forecast

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,7 +4,16 @@ class City < ActiveRecord::Base
   
   before_validation :geocode
   
-  private
+  public
+
+  def forecast_io
+    forecast = ForecastIO.forecast(self.lat, self.lon, params: { units: 'si' })
+    results = {}
+	  results[:temperature] = forecast.currently.temperature
+    results[:summary] = forecast.currently.summary
+    results[:windSpeed] = forecast.currently.windSpeed
+	  results
+  end
   
   def geocode
     places = Nominatim.search.city(self.name).limit(1)
@@ -12,5 +21,5 @@ class City < ActiveRecord::Base
       self.lat=places.first.lat
       self.lon=places.first.lon
     end
-  end
+  
 end
